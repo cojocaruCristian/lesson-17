c47d3299ecd56005ab04533e3cf15d0f491837d7
Vladislav Dobrea <vlad.d@goparrot.ai>
2019-10-19 21:24:52 +0300

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lesson-17
+
+1. Install Jest
+2. Create test file index.test.js
+3. Add npm script test: jest
+4. write tests
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 
 function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    return 'Parameters should be numbers';
-  }
   if (a === undefined || b === undefined) {
     return 'Both parameters should be present';
+  }
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'Parameters should be numbers';
   }
   return a + b;
 }
 
-
-console.log(sum(1, 2));
+module.exports = sum;
