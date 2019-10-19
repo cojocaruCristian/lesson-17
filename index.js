5900c650de82ed6d6c6d8d0419a4971ee54c363d
Vladislav Dobrea <vlad.d@goparrot.ai>
2019-10-19 19:19:09 +0300

    Added sum function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'Parameters should be numbers';
+  }
+  if (a === undefined || b === undefined) {
+    return 'Both parameters should be present';
+  }
+  return a + b;
+}
+
+
+console.log(sum(1, 2));
